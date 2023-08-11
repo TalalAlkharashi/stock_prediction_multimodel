32dc681c96bfacf207e964e77f53490dde67e963
Talal Alkharashi <86217112+TalalAlkharashi@users.noreply.github.com>
2023-08-11 15:58:11 +0100

    Delete translation.py

--- translation.py
+++
@@ -1,8 +0,0 @@
-
-for i in range(i):
-  sentence = df['sentence'][i]
-  prompt = f"""
-  Translate this sentence to proper arabic.
-  ```{sentence}```
-  """
-  df['sentence'] = df['sentence'].replace(df['sentence'][i], get_completion(prompt))
